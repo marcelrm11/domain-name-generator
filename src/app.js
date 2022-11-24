e7a9b2238928bbd62fd10d7dd9acb1676ca35f83
marcelrm11 <marcelrm11@gmail.com>
2022-11-24 11:41:09 +0000

    put code in src inside window.onload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".es", ".io"];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          console.log(pronoun.concat(adj, noun, extension));
+        }
+      }
+    }
+  }
 };
